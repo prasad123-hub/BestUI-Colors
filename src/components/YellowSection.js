7508714255c2db2110e38cb3d8549a2401af7a30
Prasad <narkhedkarprasad@gmail.com>
2021-06-08 18:53:30 +0530

    Added Responsive queries

--- src/components/YellowSection.js
+++ src/components/YellowSection.js
@@ -113,6 +113,20 @@ const ColorRange = styled.div`
   text-align: center;
   align-items: center;
   /* margin-left: 20px; */
+  @media only screen and (max-width: 1085px) {
+    grid-template-columns: repeat(5, minmax(0, 1fr));
+  }
+  @media only screen and (max-width: 991px) {
+    grid-template-columns: repeat(4, minmax(0, 1fr));
+  }
+
+  @media only screen and (max-width: 768px) {
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+  }
+
+  @media only screen and (max-width: 599px) {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
 `;
 
 const Wrap = styled.div`
